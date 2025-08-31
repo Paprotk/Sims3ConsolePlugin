c71cdc7d010a948ccc04932cf7d8c457ca568697
Arek <arek.insta@gmail.com>
2025-08-31 11:44:30 +0200

    Additional Functionality

--- Mono/Console.cs
+++ Mono/Console.cs
@@ -2,46 +2,75 @@
 using System;
 using System.Runtime.CompilerServices;
 using System.Text;
+using Sims3.Gameplay.Core;
+using Sims3.UI;
 
 /// <summary>
 /// Custom Console class for debug builds, directly invoking native console functions via unmanaged interop
 /// </summary>
 public static class Console
 {
-    [MethodImpl(MethodImplOptions.InternalCall)]
-    public static extern void Create();
+    static Console()
+    {
+        if (!Commands.sGameCommands.mCommands.ContainsKey("Clear"))
+        {
+            Commands.sGameCommands.Register("ConsoleCreate", "- Creates console window.", Commands.CommandType.General, ConsoleCheats.OnCreate, true);
+            Commands.sGameCommands.Register("ConsoleClose", "- Closes console window.", Commands.CommandType.General, ConsoleCheats.OnClose, true);
+            Commands.sGameCommands.Register("ConsoleWriteLine", "- Writes text to a console window.", Commands.CommandType.General, ConsoleCheats.OnWriteLine, true);
+            Commands.sGameCommands.Register("ConsoleClear", "- Clear console window.", Commands.CommandType.General, ConsoleCheats.OnClear, true);
+            Commands.sGameCommands.Register("ConsoleStartLogging", "- Starts logging console window.", Commands.CommandType.General, ConsoleCheats.OnStartLogging, true);
+            Commands.sGameCommands.Register("ConsoleStopLogging", "- Stops logging console window.", Commands.CommandType.General, ConsoleCheats.OnStopLogging, true);
+            Commands.sGameCommands.Register("ConsoleBeep", "- Beeps using console window.", Commands.CommandType.General, ConsoleCheats.OnBeep, true);
+        }
+    }
 
     [MethodImpl(MethodImplOptions.InternalCall)]
-    public static extern void Close();
+    public static extern bool _IsPresent();
+    
+    [MethodImpl(MethodImplOptions.InternalCall)]
+    public static extern void _Create();
 
     [MethodImpl(MethodImplOptions.InternalCall)]
-    public static extern unsafe void WriteLine(sbyte* utf8Text);
+    public static extern void _Close();
 
     [MethodImpl(MethodImplOptions.InternalCall)]
-    public static extern unsafe void StartLogging(sbyte* filenameUtf8);
+    public static extern unsafe void _WriteLine(sbyte* utf8Text);
 
     [MethodImpl(MethodImplOptions.InternalCall)]
-    public static extern unsafe void StopLogging(sbyte* filenameUtf8);
+    public static extern unsafe void _StartLogging(sbyte* filenameUtf8);
+
+    [MethodImpl(MethodImplOptions.InternalCall)]
+    public static extern unsafe void _StopLogging(sbyte* filenameUtf8);
 	
 	[MethodImpl(MethodImplOptions.InternalCall)]
-    public static extern void Clear();
+    public static extern void _Clear();
 	
 	[MethodImpl(MethodImplOptions.InternalCall)]
-    public static extern void Beep();
+    public static extern void _Beep();
     
     public static unsafe void WriteLine(string text)
     {
+        if (!_IsPresent())
+        {
+            SimpleMessageDialog.Show("Sims3ConsolePlugin","Console is not present!");
+            return;
+        }
         using (Utf8Ptr utf8Ptr = text) // Implicit conversion from string to Utf8Ptr
         {
-            WriteLine(utf8Ptr);
+            _WriteLine(utf8Ptr);
         }
     }
     
     public static unsafe void StartLogging(string filename)
     {
+        if (!_IsPresent())
+        {
+            SimpleMessageDialog.Show("Sims3ConsolePlugin","Console is not present!");
+            return;
+        }
         using (Utf8Ptr utf8Ptr = filename) 
         {
-            StartLogging(utf8Ptr);
+            _StartLogging(utf8Ptr);
         }
     }
     
@@ -49,8 +78,123 @@ public static class Console
     {
         using (Utf8Ptr utf8Ptr = filename)
         {
-            StopLogging(utf8Ptr);
+            _StopLogging(utf8Ptr);
         }
+    }
+    
+    public static void Create()
+    {
+        if (_IsPresent())
+        {
+            SimpleMessageDialog.Show("Sims3ConsolePlugin","Console is already present!");
+        }
+        else
+        {
+            _Create();
+        }
+            
+    }
+    public static void Close()
+    {
+        if (!_IsPresent())
+        {
+            SimpleMessageDialog.Show("Sims3ConsolePlugin","Console is not present!");
+        }
+        else
+        {
+            _Close();
+        }
+            
+    }
+    public static void Clear()
+    {
+        if (!_IsPresent())
+        {
+            SimpleMessageDialog.Show("Sims3ConsolePlugin","Console is not present!");
+        }
+        else
+        {
+            _Clear();
+        }
+            
+    }
+
+    public static void Beep()
+    {
+        if (!_IsPresent())
+        {
+            SimpleMessageDialog.Show("Sims3ConsolePlugin","Console is not present!");
+        }
+        else
+        {
+            _Beep();
+        }
+    }
+
+    public static class ConsoleCheats
+    {
+        public static int OnCreate(object[] parameters)
+        {
+            Create();
+            return 1;
+        }
+
+        public static int OnClose(object[] parameters)
+        {
+            Close();
+            return 1;
+        }
+        
+        public static int OnWriteLine(object[] parameters)
+        {
+            if (parameters != null && parameters.Length > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    if (parameters[i] != null)
+                    {
+                        if (sb.Length > 0) sb.Append(" ");
+                        sb.Append(parameters[i]);
+                    }
+                }
+
+                WriteLine(sb.ToString());
+            }
+            return 1;
+        }
+        
+        public static int OnClear(object[] parameters)
+        {
+            Clear();
+            return 1;
+        }
+        
+        public static int OnStartLogging(object[] parameters)
+        {
+            if (parameters != null && parameters.Length > 0 && parameters[0] != null)
+            {
+                StartLogging(parameters[0].ToString());
+            }
+            return 1;
+        }
+
+        public static int OnStopLogging(object[] parameters)
+        {
+            if (parameters != null && parameters.Length > 0 && parameters[0] != null)
+            {
+                StopLogging(parameters[0].ToString());
+            }
+            return 1;
+        }
+        
+        public static int OnBeep(object[] parameters)
+        {
+            Beep();
+            return 1;
+        }
+
     }
 }
 
@@ -111,8 +255,9 @@ public static class Console
     
     public static void StopLogging(string filename) { /* Dummy - does nothing */ }
 	
-	public static void Clear()
+	public static void Clear() { /* Dummy - does nothing */ }
 	
-	public static void Beep()
+	public static void Beep() { /* Dummy - does nothing */ }
+
 }
 #endif
